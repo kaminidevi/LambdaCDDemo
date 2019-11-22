35eb12cf3493cb2ca8c00f61f2e9fde704d7aa11
Dev-Vis <50908110+kaminidevi@users.noreply.github.com>
2019-11-22 10:47:37 +0100

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,25 @@
 import boto3
 import json
+import base64
 
-ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+    cloudwatch = boto3.client('cloudwatch')
+    response = cloudwatch.get_metric_widget_image(
+        MetricWidget = json.dumps(
+            {
+                "view": "timeSeries",
+                "stacked": False,
+                "metrics": [[ "AWS/Lambda", "Duration", "FunctionName", "indlasdemo2-saaLambdaCheckOpenDevices-KSHBXB8JWNK6" ]],
+                "width": 1054,
+                "height": 180,
+                "start": "-PT3H",
+                "end": "P0D"
+                }
+            ))
+    #OutputFormat = "png"
+    #print(response.keys())
+    return {
+        "statuscode" : 200,
+        "MetricWidgetImage" : base64.b64encode(response['MetricWidgetImage']).decode(encoding='utf-8'),
+        "isBase64Encoded": True,
+        }     
